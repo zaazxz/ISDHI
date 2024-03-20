c3b44532ab38a7e659ed97e6d5ec916dbd845c65
zaazxz <mirzaqamaruzzaman18@gmail.com>
2024-03-21 02:00:44 +0700

    feat [frontend] : Setting up tailwind responsive

---
+++ src/resources/views/auth/template/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- Tailwind CSS --}}
+    @vite('resources/css/app.css')
+
+    <title>Masuk</title>
+
+</head>
+<body>
+
+    {{-- Content --}}
+    @yield('content')
+
+    {{-- JavaScript --}}
+    <script src="{{ asset('js/jquery/jquery.js') }}"></script>
+    @vite('public/js/script.js')
+
+</body>
+</html>
