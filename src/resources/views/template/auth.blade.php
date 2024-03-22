16a31097e359a05d40d16565ec87ef860e9de16d
zaazxz <mirzaqamaruzzaman18@gmail.com>
2024-03-23 01:34:17 +0700

    fix [frontend] : Fixing bug and change from tailwind to bootstrap

---
+++ src/resources/views/template/auth.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Masuk</title>
+
+    {{-- CSS Mazer Assets --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main/app-dark.css') }}">
+
+    {{-- Favicon --}}
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.png') }}" type="image/png">
+
+</head>
+
+<body>
+
+    {{-- Content --}}
+    @yield('content')
+
+    {{-- Javascript Mazer Assets --}}
+    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+
+</body>
+
+</html>
